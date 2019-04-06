82c754e6cc43226225e5492ff154875ee91f37f9
Jesse Gutierrez <jesse0707@gmail.com>
2019-04-06 12:54:42 -0600

    New Files and code

--- Loops.py
+++ Loops.py
@@ -1,1 +1,35 @@
-#a for Loop is used for iterat
+#a for Loop is used for iterate over a sequence (that is either a list, tuple, dictionary, set or string).
+
+empresas = ['IBM', 'DELL', 'HP', 'INTEL']
+
+#Simple for loop
+for posiblesTrabajos in empresas:
+    print(f'Posibles puestos en: {posiblesTrabajos}')
+print('***Simple Loop***')
+
+
+#Break that stops when the condition is found
+for posiblesTrabajos2 in empresas:
+    if posiblesTrabajos2 == 'HP':
+        break
+    print(f'Posibles puestos en: {posiblesTrabajos2}')
+print('***Loop and Break***')
+
+#Continue
+for posiblesTrabajos3 in empresas:
+    if posiblesTrabajos3 == 'IBM':
+        continue
+    print(f'Posibles puestos en: {posiblesTrabajos3}')
+print('***Loop and Continue***')
+
+#Range
+for i in range(0, 5):
+    print(f'Numeros: {i}')
+print('***loop and Range***')
+
+#While Loops: execute a set of statements as long as a conditions is true
+conteo = 0
+while conteo <= 5:
+    print(f'Numeros: {conteo}')
+    conteo += 1
+print('***loops and while***')
