3de75350464c35af03bd5ee715409fe03361f227
jessegutierrez <jesse007@gmail.com>
2019-04-08 01:58:56 -0500

    minor changes

--- Examples/BubbleSort.py
+++ Examples/BubbleSort.py
@@ -1,10 +1,12 @@
 def bSort(listado):
+    print(listado)
     for a in range(len(listado)-1, 0, -1):
         for b in range(a):
             if listado[b] > listado[b+1]:
                 tmp = listado[b]
                 listado[b] = listado[b+1]
                 listado[b+1] = tmp
+                print(listado)
 
 
 listado = [11, 4, 53, 8, 21, 9, 54, 89, 74, 2, 1]
